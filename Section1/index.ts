6f7ebfb22a317b3792a9a05a03e7818ce72568ff
“sowmyakota” <venkatasowmyasrikota98@gmail.com>
2023-02-18 15:41:50 -0800

    Modified index.ts

--- Section1/index.ts
+++ Section1/index.ts
@@ -9,13 +9,15 @@ interface Todo {
 
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
-  const ID = todo.id;
+  const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
-
-  console.log(`
-  The Todo with ID: ${ID}
-  Has a tilte of: ${title}
-  IS it finished: ${finished}
-  `);
+  const completed = todo.completed;
+  logTodo(id, title, completed);
 });
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a tilte of: ${title}
+    IS it finished: ${completed}
+    `);
+};
