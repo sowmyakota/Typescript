1673b6cdc4ee2e293a9d04fdadfda3248b3f0f75
“sowmyakota” <venkatasowmyasrikota98@gmail.com>
2023-02-18 22:16:02 -0800

    Added functions in interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -2,18 +2,27 @@ interface Vechile {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
   year: 200,
   broken: true,
+  summary(): string {
+    return `Name ${this.name}`;
+  },
 };
 
 const printVechile = (vehicle: Vechile): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken: ${vehicle.broken}`);
+  console.log(`${vehicle.summary()}`);
+};
+
+const printSummary = (vehicle: Vechile): void => {
+  console.log(`${vehicle.summary()}`);
 };
 
 printVechile(oldCivic);
